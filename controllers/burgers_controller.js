10c8a0ed74a2ba45dd8ada8f7b6e43f4db52959b
Michael Nguyen <thinknownotforever@gmail.com>
2019-01-14 23:12:02 -0800

    page is now working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
   router.post("/api/burgers", function(req, res) {
     console.log("route hit")
     burger.insertOne([
-      "burger_name"
+      "burger_name", "devoured"
     ], [
-      req.body.burger_name
+      req.body.burger_name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,10 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
+    $(".devour-button").on("click", function(event) {
       var id = $(this).data("id");
-      var newEaten = $(this).data("neweaten");
   
       var newState = {
-        devoured: newEaten
+        devoured: true
       };
   
       // Send the PUT request.
@@ -14,14 +13,14 @@ $(function() {
         data: newState
       }).then(
         function() {
-          console.log("devoured", newEaten);
+          console.log("devoured");
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
   
-    $(".create-form").on("submit", function(event) {
+    $(".addBurgerButton").on("click", function(event) {
       // Make sure to preventDefault on a submit event.
       console.log("clicked?")
       event.preventDefault();
