f00548e6e2576159fdb74864b32e25c32d074cb2
Michael Nguyen <thinknownotforever@gmail.com>
2019-01-18 12:08:38 -0800

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
+var connectionInfo;
+if (process.env.JAWSDB_URL) {
+  connectionInfo = process.env.JAWSDB_URL;
+} else {
+  connectionInfo = {
+  host: process.env,
   port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
-  });
+  };
+}
 
+var connection = mysql.createConnection(connecitoninfo);
 // Make connection.
 connection.connect(function(err) {
   if (err) {
